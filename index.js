996ea3b76b61f864db092032a5a79cf0816fc7a6
Chun Tian (binghe) <binghe.lisp@gmail.com>
2024-07-17 12:35:29 +1000

    Change language name to "sml" instead of "standard-ml"

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 try {
-  module.exports = require("./build/Release/tree_sitter_standard_ml_binding");
+  module.exports = require("./build/Release/tree_sitter_sml_binding");
 } catch (error) {
   try {
-    module.exports = require("./build/Debug/tree_sitter_standard_ml_binding");
+    module.exports = require("./build/Debug/tree_sitter_sml_binding");
   } catch (_) {
     throw error
   }
